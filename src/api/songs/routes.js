3e129b79ca791ac49fb784b7a7a78e2b7c1a4c28
mishbahulm <munir.m3@gmail.com>
2021-07-19 12:27:34 +0700

    remove option jwt on songs - routes; add column owner on songs table

--- src/api/songs/routes.js
+++ src/api/songs/routes.js
@@ -3,41 +3,26 @@ const routes = (handler) => [
     method: 'POST',
     path: '/songs',
     handler: handler.postSongHandler,
-    options: {
-      auth: 'notesapp_jwt',
-    },
   },
   {
     method: 'GET',
     path: '/songs',
     handler: handler.getSongsHandler,
-    options: {
-      auth: 'notesapp_jwt',
-    },
   },
   {
     method: 'GET',
     path: '/songs/{songId}',
     handler: handler.getSongByIdHandler,
-    options: {
-      auth: 'notesapp_jwt',
-    },
   },
   {
     method: 'PUT',
     path: '/songs/{songId}',
     handler: handler.putSongByIdHandler,
-    options: {
-      auth: 'notesapp_jwt',
-    },
   },
   {
     method: 'DELETE',
     path: '/songs/{songId}',
     handler: handler.deleteSongByIdHandler,
-    options: {
-      auth: 'notesapp_jwt',
-    },
   },
 ];
 
