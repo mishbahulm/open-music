f213521084a754bf5d414167f5877343da73b4f0
mishbahulm <munir.m3@gmail.com>
2021-07-19 09:54:08 +0700

    Use users plugin in hapi server

--- src/server.js
+++ src/server.js
@@ -1,13 +1,21 @@
 require('dotenv').config();
 const Hapi = require('@hapi/hapi');
+const ClientError = require('./exceptions/ClientError');
+
+// songs
 const songs = require('./api/songs');
 // const SongsService = require('./services/inMemory/SongsService');
 const SongsService = require('./services/postgres/SongsService');
 const SongValidator = require('./validator/songs');
-const ClientError = require('./exceptions/ClientError');
+
+// users
+const users = require('./api/users');
+const UsersService = require('./services/postgres/UsersService');
+const UsersValidator = require('./validator/users');
 
 const init = async () => {
   const songsService = new SongsService();
+  const usersService = new UsersService();
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -19,13 +27,22 @@ const init = async () => {
     },
   });
 
-  await server.register({
-    plugin: songs,
-    options: {
-      service: songsService,
-      validator: SongValidator,
+  await server.register([
+    {
+      plugin: songs,
+      options: {
+        service: songsService,
+        validator: SongValidator,
+      },
     },
-  });
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
+      },
+    },
+  ]);
 
   server.ext('onPreResponse', (request, h) => {
     // mendapatkan konteks response dari request
